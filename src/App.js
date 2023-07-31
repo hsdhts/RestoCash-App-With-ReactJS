b8c0459f8bce58ab283a937458fb6cf0dccfeecc
husada20 <husadahutasoit@gmail.com>
2023-07-31 15:40:40 +0700

    update add to chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Hasil, ListCategories, Menus, NavbarComponents } from './components/Inde
 // import React, { Component } from 'react'
 import { API_URL } from './utils/constants';
 import axios from 'axios';
+import swal from 'sweetalert'
 
 export default class App extends Component {
   constructor(props) {
@@ -11,41 +12,117 @@ export default class App extends Component {
   
     this.state = {
       menus: [],
-      categoriYangDipilih: 'Makanan'
+      categoriYangDipilih: 'Makanan',
+      charts : []
     };
   }
   
   componentDidMount() {
     axios
       .get(API_URL + "products?category.nama=" + this.state.categoriYangDipilih)
-      .then(res => {
-        // console.log("Response : ", res)
+      .then((res) => {
         const menus = res.data;
         this.setState({ menus });
       })
-      .catch(error => {
-        console.log("Terjadi error: ", error);
+      .catch((error) => {
+        console.log("Error yaa ", error);
       });
+
+    axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  }
+
+  componentDidUpdate(prevState) {
+    if(this.state.keranjangs !== prevState.keranjangs) {
+      axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+    }
   }
 
   changeCategory = (value) => {
     this.setState({
       categoriYangDipilih: value,
-      menus: []
-    })
-
-    axios
-    .get(API_URL + "products?category.nama=" + value)
-    .then(res => {
-      const menus = res.data;
-      this.setState({ menus });
-    })
-    .catch(error => {
-      console.log("Terjadi error: ", error);
+      menus: [],
     });
 
+    axios
+      .get(API_URL + "products?category.nama=" + value)
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  };
 
-  }
+  masukKeranjang = (value) => {
+    axios
+      .get(API_URL + "keranjangs?product.id=" + value.id)
+      .then((res) => {
+        if (res.data.length === 0) {
+          const keranjang = {
+            jumlah: 1,
+            total_harga: value.harga,
+            product: value,
+          };
+
+          axios
+            .post(API_URL + "keranjangs", keranjang)
+            .then((res) => {
+              swal({
+                title: "Sukses Masuk Keranjang",
+                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1500,
+              });
+            })
+            .catch((error) => {
+              console.log("Error yaa ", error);
+            });
+        } else {
+          const keranjang = {
+            jumlah: res.data[0].jumlah + 1,
+            total_harga: res.data[0].total_harga + value.harga,
+            product: value,
+          };
+
+          axios
+            .put(API_URL + "keranjangs/" + res.data[0].id, keranjang)
+            .then((res) => {
+              swal({
+                title: "Sukses Masuk Keranjang",
+                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1500,
+              });
+            })
+            .catch((error) => {
+              console.log("Error yaa ", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  };
+  
   
   render() {
     const { menus, categoriYangDipilih} = this.state;
@@ -65,6 +142,7 @@ export default class App extends Component {
        <Menus
       key={menu.id}
        menu={menu}
+       masukKeranjang= {this.masukKeranjang}
        />
        ))}
         </Row>
