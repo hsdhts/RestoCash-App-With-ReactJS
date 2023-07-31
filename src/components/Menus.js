490a94ff90fda9bdf9d4bb8493f6eb396c752bff
husada20 <husadahutasoit@gmail.com>
2023-07-31 10:06:54 +0700

    menampilkan menu product

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,10 +1,24 @@
-import React from 'react'
+// import React from 'react'
+import {Col, Card} from 'react-bootstrap'
+import {numberWithCommas} from '../utils/utils'
 
 const Menus = ({menu}) => {
   return (
-    <div>
-      <h2>{menu.nama}</h2>  
-    </div>
+    // <div>
+    //   <h2>{menu.nama}</h2>  
+    // </div>
+    <Col md={4} xs={6} className='mb-4'>
+    <Card className='shadow'>
+      <Card.Img variant="top" src={"assets/images/"+ menu.category.nama.toLowerCase()+ "/" + menu.gambar} />
+      <Card.Body>
+        <Card.Title>{menu.nama} <strong>{menu.kode}</strong></Card.Title>
+        <Card.Text>
+          Rp. { numberWithCommas(menu.harga)}
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
+    </Col>
   )
 }
 
